e9f65c6f59b0fbf0096278c0642fe47b077c3e15
saadansaryy <ansarisaad990@gmail.com>
2022-12-02 19:30:45 +0530

    comments

--- db/connector.js
+++ db/connector.js
@@ -15,8 +15,9 @@ mongoose
   .catch((err) => {
     console.log("error while connection", err);
   });
-let collection_connection = mongoose.model("bookmovietickets", bookMovieSchema);
-let user_connection = mongoose.model("User", UserSchema);
+
+let collection_connection = mongoose.model("bookmovietickets", bookMovieSchema); // bookmovietickets Collection
+let user_connection = mongoose.model("User", UserSchema); // User Collection
 
 exports.connection = collection_connection;
 exports.Userconnection = user_connection;
