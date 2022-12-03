7912506630b5d5e54d4222a7871d041363eab63f
saadansaryy <ansarisaad990@gmail.com>
2022-12-03 19:28:03 +0530

    added db name in mongoURI

--- db/connector.js
+++ db/connector.js
@@ -1,7 +1,7 @@
 const mongodb = require("mongodb");
 
 const mongoURI =
-  "mongodb+srv://saadansari:mongodbfa9@bms.mg6hlxg.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://saadansari:mongodbfa9@bms.mg6hlxg.mongodb.net/bookmyshow?retryWrites=true&w=majority";
 
 let mongoose = require("mongoose");
 const { bookMovieSchema } = require("../models/bookMovie");
